d9a02800974debe17c06546b8205531d4408e470
felipes <felipes@felipe>
2015-09-26 22:13:20 -0300

    adicionado opções de salvar/abrir

---
+++ IONDULA/src/br/arquivo/arquivo.java
@@ -0,0 +1,86 @@
+package br.arquivo;
+
+import br.janela.CompiladorGUI;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+/**
+ *
+ * @author felipes
+ */
+public class arquivo implements ActionListener {
+
+    CompiladorGUI cp;
+
+    public arquivo(CompiladorGUI cp) {
+        this.cp = cp;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent arq) {
+        switch (arq.getActionCommand()) {
+            case "Abrir": {
+                Abrir();
+                break;
+            }
+            case "Salvar": {
+                Salvar();
+                break;
+            }
+        }
+    }
+
+    public void Abrir() {
+        CompiladorGUI gui = new CompiladorGUI();
+
+        JFileChooser fc = new JFileChooser();
+        //Define que apenar diretórios podem ser selecionados
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("IONDULA FILES", "ion", "IONDULA");
+        fc.setFileFilter(filter);
+        fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        //Exibe o diálogo. Deve ser passado por parâmetro o JFrame de origem.
+        fc.showOpenDialog(gui);
+        //Captura o objeto File que representa o diretório selecionado.
+        File file = fc.getSelectedFile();
+
+        cp.cleanEditor();
+        try {
+            FileReader reader = new FileReader(file);
+            BufferedReader input = new BufferedReader(reader);
+            String linha;
+            while ((linha = input.readLine()) != null) {
+                cp.setEditor(linha);
+            }
+            input.close();
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+        }
+    }
+
+    public void Salvar() {
+
+        String txtSemEspaco = cp.getEditor().getText().replaceAll("\n", "¬");
+        txtSemEspaco = txtSemEspaco.replaceAll(" ", "¢");
+        JFileChooser chooser = new JFileChooser();
+        chooser.setCurrentDirectory(new File("/home/me/Documents"));
+        int retrival = chooser.showSaveDialog(null);
+        if (retrival == JFileChooser.APPROVE_OPTION) {
+            try {
+                FileWriter fw = new FileWriter(chooser.getSelectedFile() + ".ion");
+                fw.write(txtSemEspaco.toString());
+                fw.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
--- IONDULA/src/br/janela/CompiladorGUI.java
+++ IONDULA/src/br/janela/CompiladorGUI.java
@@ -1,7 +1,12 @@
 package br.janela;
 
+import br.arquivo.arquivo;
 import br.listener.Lexico;
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.DefaultTableModel;
 
 public class CompiladorGUI extends javax.swing.JFrame {
@@ -16,9 +21,13 @@ public class CompiladorGUI extends javax.swing.JFrame {
         model = (DefaultTableModel) console.getModel();
         
         lal = new Lexico(this);
-        
+        arquivo arq = new arquivo(this);
         Analisar.addActionListener(lal);
         Analisar.setActionCommand("Analisar");
+        Abrir.addActionListener(arq);
+        Abrir.setActionCommand("Abrir");
+        Salvar.addActionListener(arq);
+        Salvar.setActionCommand("Salvar");
     }
 
     public void setConsole(int c, String t, int l, String i){
@@ -33,8 +42,12 @@ public class CompiladorGUI extends javax.swing.JFrame {
         return txtAreaEditor;
     }
 
-    public void setEditor(JTextArea txtAreaEditor) {
-        this.txtAreaEditor = txtAreaEditor;
+    public void setEditor(String linha) {
+        this.txtAreaEditor.append(linha);
+    }
+    
+    public void cleanEditor(){
+        txtAreaEditor.setText("");
     }
 
     @SuppressWarnings("unchecked")
@@ -48,9 +61,15 @@ public class CompiladorGUI extends javax.swing.JFrame {
         console = new javax.swing.JTable();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
+        Abrir = new javax.swing.JMenuItem();
+        Salvar = new javax.swing.JMenuItem();
+        jMenuItem5 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("I.O.N.D.U.L.A - Insira o Nome de Uma Linguagem Aqui");
 
         txtAreaEditor.setColumns(20);
         txtAreaEditor.setRows(5);
@@ -73,10 +92,47 @@ public class CompiladorGUI extends javax.swing.JFrame {
         ));
         jScrollPane3.setViewportView(console);
 
-        jMenu1.setText("File");
+        jMenu1.setText("Arquivo");
+
+        Abrir.setText("Abrir");
+        Abrir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                AbrirActionPerformed(evt);
+            }
+        });
+        jMenu1.add(Abrir);
+
+        Salvar.setText("Salvar");
+        jMenu1.add(Salvar);
+
+        jMenuItem5.setText("Sair");
+        jMenuItem5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem5ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem5);
+
         jMenuBar1.add(jMenu1);
 
-        jMenu2.setText("Edit");
+        jMenu2.setText("Ajuda");
+
+        jMenuItem1.setText("Ajuda");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuItem1);
+
+        jMenuItem2.setText("Sobre");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuItem2);
+
         jMenuBar1.add(jMenu2);
 
         setJMenuBar(jMenuBar1);
@@ -85,9 +141,9 @@ public class CompiladorGUI extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 1047, Short.MAX_VALUE)
+            .addComponent(jScrollPane1)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
+                .addGap(0, 927, Short.MAX_VALUE)
                 .addComponent(Analisar, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE))
             .addComponent(jScrollPane3)
         );
@@ -110,6 +166,22 @@ public class CompiladorGUI extends javax.swing.JFrame {
 
        
     }//GEN-LAST:event_AnalisarActionPerformed
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+    JOptionPane.showMessageDialog(rootPane, String.format("<html><center>Para maiores informações sobre a linguagem,<br> acesse o manual disponivel em:<br>http://bit.do/IONDULA</center></html>"), "Ajuda", 3);
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
+
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+    JOptionPane.showMessageDialog(rootPane, String.format("<html><center>Desenvolvido por:<br>Felipe Garcia de Souza<br>Luiz Veríssimo da Silva de Souza<br><br>Copyright © 2015</center></html>"), "Sobre", JOptionPane.PLAIN_MESSAGE);
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
+
+    private void AbrirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AbrirActionPerformed
+
+    }//GEN-LAST:event_AbrirActionPerformed
+
+    private void jMenuItem5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem5ActionPerformed
+    System.exit(0);
+    }//GEN-LAST:event_jMenuItem5ActionPerformed
 
     
     public static void main(String args[]) {
@@ -144,11 +216,16 @@ public class CompiladorGUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem Abrir;
     private javax.swing.JButton Analisar;
+    private javax.swing.JMenuItem Salvar;
     private javax.swing.JTable console;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem5;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JTextArea txtAreaEditor;
