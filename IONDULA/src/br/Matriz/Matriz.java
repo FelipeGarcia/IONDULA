7462cb9ad01d22071f91ab341604b283d69368f3
Felipe <Felipe@172.16.15.70>
2015-09-28 21:22:39 -0300

    inicio sintatico

--- IONDULA/src/br/Matriz/Matriz.java
+++ IONDULA/src/br/Matriz/Matriz.java
@@ -4,5 +4,16 @@ package br.Matriz;
 
 public class Matriz {
    
-    int[][] matriz = new int[27][41];
+    static int[][] matriz = new int[27][41];
+    
+    public void popula(){
+        matriz[0][8] = 1;
+        matriz[1][2] = 2;
+        matriz[2][22] = 2;
+        matriz[3][23] = 3;
+        matriz[4][2] = 5;
+        matriz[4][22] = 4;
+        
+    }
+    
 }
